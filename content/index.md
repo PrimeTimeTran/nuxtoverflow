a1cb7292b49177b9387638bc48ef6e9d4983fee4
Loi Tran <primetimetran@gmail.com>
2023-10-29 12:46:02 -0400

    Add form

--- content/index.md
+++ content/index.md
@@ -1,3 +1,5 @@
 # Loi Tran's Vue Blog
 
 Welcome. Thank you for taking the time to visit. Please have a look around.
+
+Sign up for my news letter below.
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,98 @@
 <template>
   <div class="pt-24">
     <ContentDoc class="prose" />
+    <!-- <form
+      netlify
+      name="contact"
+      method="POST"
+      onSubmit="submit"
+      class="bg-gray-200 p-16 mt-16 rounded flex flex-col justify-center align-middle"
+    >
+      <label
+        name="name"
+        class="block w-1/2 place-self-center"
+      >
+        <span class="text-gray-700">Name</span>
+        <input
+          id="name"
+          name="name"
+          placeholder="Jane Doe"
+          class="form-input block w-full text-black p-3 mt-3 rounded"
+        />
+      </label>
+      <label
+        name="email"
+        class="block w-1/2 place-self-center mt-3"
+      >
+        <span class="text-gray-700">Email</span>
+        <input
+          id="email"
+          name="email"
+          placeholder="jane@gmail.com"
+          class="form-input block w-full text-black p-3 mt-3 rounded"
+        />
+      </label>
+      <div class="w-1/2 place-self-center">
+        <button
+          type="submit"
+          class="text-white w-full mt-8 bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mb-2 place-self-center"
+        >
+          Submit
+        </button>
+      </div>
+    </form> -->
+
+    <h2>Contact Form</h2>
+    <form
+      netlify
+      method="post"
+      name="Contact"
+      action="/thankyou"
+      @submit.prevent="sendForm"
+      class="bg-gray-200 p-16 mt-16 rounded flex flex-col justify-center align-middle"
+    >
+      <input
+        class="hidden"
+        name="hpfield"
+      />
+      <p>
+        <label
+          for="name"
+          class="text-gray-800"
+        >
+          Name:
+        </label>
+        <input
+          type="text"
+          name="name"
+          id="name"
+          class="form-input block w-full text-black p-3 mt-3 rounded"
+        />
+      </p>
+      <p>
+        <label
+          for="email"
+          class="text-gray-800"
+        >
+          Email:
+        </label>
+        <input
+          type="email"
+          name="email"
+          id="email"
+          class="form-input block w-full text-black p-3 mt-3 rounded"
+        />
+      </p>
+      <div data-netlify-recaptcha="true"></div>
+      <p>
+        <button
+          type="submit"
+          class="text-white w-full mt-8 bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mb-2 place-self-center"
+        >
+          Submit
+        </button>
+      </p>
+    </form>
   </div>
 </template>
 
@@ -10,4 +102,24 @@ useHead({
     { src: 'https://identity.netlify.com/v1/netlify-identity-widget.js' },
   ],
 })
+// https://snipcart.com/blog/netlify-forms
+async function sendForm(e) {
+  let formData = new FormData()
+  console.log({
+    name: e.target[1].value,
+    email: e.target[2].value,
+  })
+
+  formData.append('name', e.target[1].value)
+  formData.append('email', e.target[2].value)
+  formData.append('form-name', 'Contact')
+  var options = { method: 'POST', body: formData, content: formData }
+
+  let resp = await fetch('/', options)
+  console.log({
+    name: formData.get('name'),
+    email: formData.get('email'),
+  })
+  alert('Submitted!')
+}
 </script>
